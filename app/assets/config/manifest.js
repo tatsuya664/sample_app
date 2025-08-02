cac42491cae2a2af8473d55af43dec3f2dec6f54
Tatsuya Kawashima <tyler.tyler86468@gmail.com>
2025-08-03 01:05:42 +0900

    Fix asset pipeline conflict between Sprockets and cssbundling

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,4 @@
+// app/assets/config/manifest.js
+
 //= link_tree ../images
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
-//= link application.css
