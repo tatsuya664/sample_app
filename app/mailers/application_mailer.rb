d375e7ec542cf3548ef992a7f00786ae59549716
Tatsuya Kawashima <tyler.tyler86468@gmail.com>
2025-08-03 02:52:18 +0900

    Update default from email address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "noreply@sandbox1a15f7dbc702446f93a71117b9d2e73a.mailgun.org"
   layout "mailer"
 end
