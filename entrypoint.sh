17e2b0c6c59e4252ef70ea31e2a38368eaf0d72d
Tatsuya Kawashima <tyler.tyler86468@gmail.com>
2026-01-22 01:35:06 +0900

    Update DB config to Supabase, update README, and optimize entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# データベースのマイグレーションを実行
-bundle exec rails db:migrate
+# データベースのマイグレーションを実行(2026/0122廃止)
+# bundle exec rails db:migrate
 
 # Pumaサーバーを起動
 bundle exec puma -C config/puma.rb
